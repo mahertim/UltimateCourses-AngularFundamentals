eed5faf448648d8fecb2ae32c0c75fbfd489bb1d
Tim Maher <tim@timmaher.dev>
2020-01-03 15:54:54 -0500

    Presentational (dumb) components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-count',
+  templateUrl: `
+    <div>
+      Count Component
+    </div>
+  `,
+})
+export class PassengerCountComponent {
+  constructor() {}
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-detail',
+  templateUrl: `
+    <div>
+      Detail Component
+    </div>
+  `,
+})
+export class PassengerDetailComponent {
+  constructor() {}
+}
