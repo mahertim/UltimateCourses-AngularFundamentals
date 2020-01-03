45f3c4af89dd8b59cad23ff048d38672d62b033c
Tim Maher <tim@timmaher.dev>
2020-01-03 16:32:49 -0500

    Dynamic @Input values with *ngFor

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-detail',
-  templateUrl: `
-    <div>
-      Detail Component
-    </div>
-  `,
+  styleUrls: ['./passenger-detail.component.scss'],
+  templateUrl: './passenger-detail.component.html',
 })
 export class PassengerDetailComponent {
+  @Input()
+  detail: Passenger | null = null;
   constructor() {}
 }
