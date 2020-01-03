2657e87231a205e2b48b8e987219b389d9cd4551
Tim Maher <tim@timmaher.dev>
2020-01-03 15:32:58 -0500

    Creating a container (smart) component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,57 +1,12 @@
 import { Component } from '@angular/core';
 
-interface Child {
-  name: string;
-  age: number;
-}
-
-interface Passenger {
-  id: number;
-  fullname: string;
-  checkedIn: boolean;
-  checkedInDate?: number;
-  children?: Child[];
-}
-
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  templateUrl: 'app.component.html',
+  template: `
+    <div class="app">
+      <passenger-dashboard></passenger-dashboard>
+    </div>
+  `,
 })
-export class AppComponent {
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-    },
-    {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      children: [
-        { name: 'Ted', age: 12 },
-        { name: 'Chloe', age: 7 },
-      ],
-    },
-    {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-    },
-    {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-      children: [{ name: 'Jessica', age: 12 }],
-    },
-    {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-    },
-  ];
-}
+export class AppComponent {}
