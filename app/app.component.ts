e1110150e79eb245852c4e870c5a21f14f7ea422
Tim Maher <tim@timmaher.dev>
2020-01-03 11:33:46 -0500

    Property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,21 +5,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title + '!' }}
-      <div>
-        {{ numberOne + numberTwo }}
-      </div>
-      <div>
-        {{ isHappy ? ':)' : ':(' }}
-      </div>
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo" />
+      <input type="text" [value]="name" />
+      <div>{{ name }}</div>
     </div>
   `,
 })
 export class AppComponent {
   title: string;
-  isHappy: boolean = true;
-  numberOne: number = 1;
-  numberTwo: number = 2;
+  name: string = 'Todd';
+  logo: string = 'img/angular_logo.png';
   constructor() {
     this.title = 'Ultimate Angular';
   }
