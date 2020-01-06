d2afd24a53e70322177cf97aab78af83d707bccb
Tim Maher <tim@timmaher.dev>
2020-01-06 09:02:53 -0500

    Immutable state changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -50,11 +50,18 @@ export class PassengerDashboardComponent implements OnInit {
     ];
   }
 
-  handleRemove(event: any) {
-    console.log(event);
+  handleRemove(event: Passenger) {
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    });
   }
 
-  handleEdit(event: any) {
-    console.log(event);
+  handleEdit(event: Passenger) {
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    });
   }
 }
