68c710d55227fb0a54a42248a653bc54f8f13262
Tim Maher <tim@timmaher.dev>
2020-01-06 10:54:44 -0500

    Http put, delete with immutable state

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -20,18 +20,24 @@ export class PassengerDashboardComponent implements OnInit {
       .subscribe((data: Passenger[]) => (this.passengers = data));
   }
 
-  handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger: Passenger) => {
-      return passenger.id !== event.id;
-    });
+  handleEdit(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === data.id) {
+            passenger = Object.assign({}, passenger, data);
+          }
+          return passenger;
+        });
+      });
   }
 
-  handleEdit(event: Passenger) {
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      if (passenger.id === event.id) {
-        passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
+  handleRemove(event: Passenger) {
+    this.passengerService.removePassenger(event).subscribe(() => {
+      this.passengers = this.passengers.filter((passenger: Passenger) => {
+        return passenger.id !== event.id;
+      });
     });
   }
 }
